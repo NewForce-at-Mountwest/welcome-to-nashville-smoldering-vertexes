ce2cffc7a4371f4901a893760ef6087edfa9b00c
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-30 14:10:39 -0400

    no coding but plans laid out somewhat

--- concerts.js
+++ concerts.js
@@ -1,3 +1,13 @@
-//fetch api from ticket master link
+//ticket master url https://app.ticketmaster.com/discovery/v2/
+//api key Hftj2vZHCGI9rWySOyg1NifFCLWd7KMA
 
-//
+
+
+
+
+
+//add an event listener for the search text in id="concerts-search" button id="concerts-search-btn".  might need an if/else with statement for value typed into search.
+
+//fetch info from ticket master
+
+//tailor fetched info to go with add event listener 
