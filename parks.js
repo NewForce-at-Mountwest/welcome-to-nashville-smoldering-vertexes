47d032181c3eb1d737c177413a6f4bd1a11f39a5
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-04-30 15:58:27 -0400

    works with basic functionality

--- parks.js
+++ parks.js
@@ -1,25 +1,32 @@
 document.querySelector(".parks-div").innerHTML = `<input type="text" class="search-input" id="parks-search" placeholder="parks by feature">
 <button class="search-btn" id="parks-search-btn">Search</button>`
-if (parks-search.value !== undefined){
-    const parkInput = parks-search.value
 
-    fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${parkInput}=Yes`, {
-    "$$app_token": "13nl5nk63d0acrwo6ufqryne8rgqfwsv17seprnfwqyxeliqix"
-    })
+document.querySelector("#parks-search-btn").addEventListener("click", function(){
+    const parkInputValue = document.querySelector("#parks-search").value
+    if(parkInputValue !== undefined){
+        parkInput = parkInputValue.replace(" ", "_")
+        console.log(parkInput)
+        resultsPrinter()
+    
+    fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${parkInput}=Yes`)
         .then(r => r.json())
         .then(parksArray => {
             console.log(parksArray)
+            let counter = 1;
             parksArray.forEach(park => {
-                console.log(park)
-                if (park.community_center === "Yes"){
-                    // const parksNameHTMLString
-                    document.querySelector("#results-textarea").innerHTML += `<p>${park.park_name}</p>` 
-
-                }
+                document.querySelector("#results-div").innerHTML += `<p>${counter}. ${park.park_name}: <em>${park.mapped_location_address}<em></p>`
+                counter++; 
             })
             
         })
-}
+    }
+    })
 
+
+    // fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${parkInput}=Yes`,{
+    //     // headers: {
+    //     //     "X-App-Token": "d74bdg9o2r4jdove1ww0di1t9"
+    //     // }
+    // })
     // key ID: d74bdg9o2r4jdove1ww0di1t9
     // key secret: 13nl5nk63d0acrwo6ufqryne8rgqfwsv17seprnfwqyxeliqix
