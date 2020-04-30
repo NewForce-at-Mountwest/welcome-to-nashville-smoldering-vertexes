e59a4f2ea7f998e8ec6b92330691d3854978d0ab
Your Name <you@example.com>
2020-04-30 14:08:36 -0400

    added breweries api working

--- breweries.js
+++ breweries.js
@@ -1,1 +1,30 @@
 // https://www.openbrewerydb.org/
+ 
+//fetching the information
+fetch("https://api.openbrewerydb.org/breweries?by_city=nashville")
+  .then((breweries) => breweries.json())
+  .then((parsedBrewriesArray) => {
+    console.log(parsedBrewriesArray);
+
+    parsedBrewriesArray.forEach(breweries => {
+        
+      if(parsedBrewriesArray.breweries){
+
+      }
+      if(){
+
+      }
+      if(){
+
+      }
+      if(){
+
+      }
+      else {
+
+      }
+
+    });
+
+
+  });
