3911cc5fd6b78092a58e2626d632a0f29ebe1374
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-05-01 11:42:51 -0400

    getting specific search results printed to container

--- concerts.js
+++ concerts.js
@@ -7,12 +7,42 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=
         console.log(eventInfoArray._embedded.events)
         document.querySelector("#concertSearch-btn").addEventListener("click", function(){
             // console.log("you clicked search button")
-            const concertSearchValue = document.querySelector("#concertSearch").value 
+            const concertSearchValue = document.querySelector("#concertSearch").value;
             // console.log(concertSearchValue)
             //         // Print 'results' to the DOM
-                document.querySelector("#results").innerHTML =
-                `<h4>Concerts in Nashville</h4>
-        //         <p>${eventInfoArray._embedded.events}</p>`
+            for(i=0; i < eventInfoArray._embedded.events.length; i++){
+                document.querySelector("#results").innerHTML +=
+                `<p>${eventInfoArray._embedded.events[i].name}</p>
+                <p>${eventInfoArray._embedded.events[i].url}</p>`
+            }
+            if(concertSearchValue === "Lady Antebellum"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[0].name}</p>
+                <p>${eventInfoArray._embedded.events[0].url}</p>`
+            }else if(concertSearchValue === "Rocketman" || concertSearchValue=== "Nashville Symphony"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[1].name}</p>
+                <p>${eventInfoArray._embedded.events[1].url}</p>` 
+            }else if(concertSearchValue === "Harry Styles"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[2].name}</p>
+                <p>${eventInfoArray._embedded.events[2].url}</p>`
+            }else if(concertSearchValue === "Kenny Chesney"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[3].name}</p>
+                <p>${eventInfoArray._embedded.events[3].url}</p>`
+            }else if(concertSearchValue === "Motley Crue" || concertSearchValue === "Def Leppard" || concertSearchValue === "Joan Jett"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[4].name}</p>
+                <p>${eventInfoArray._embedded.events[4].url}</p>`
+            }else if(concertSearchValue === "Alanis Morissette"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[5].name}</p>
+                <p>${eventInfoArray._embedded.events[5].url}</p>`
+            }else if(concertSearchValue === "Halsey"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[6].name}</p>
+                <p>${eventInfoArray._embedded.events[6].url}</p>`
+            }else if(concertSearchValue === "Jimmy Buffett"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[7].name}</p>
+                <p>${eventInfoArray._embedded.events[7].url}</p>`
+            }else if(concertSearchValue === "Justin Bieber"){
+                document.querySelector("#results").innerHTML = `<p>${eventInfoArray._embedded.events[8].name}</p>
+                <p>${eventInfoArray._embedded.events[8].url}</p>`
+            }
         });
     });
 
@@ -22,15 +52,6 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=
 
 
 //add an event listener for the search text in id="concertSearch" button id="concertSearch-btn".  might need an if/else with statement for value typed into search.
-// document.querySelector("#concertSearch-btn").addEventListener("click", function(){
-//     // console.log("you clicked search button")
-//     const concertSearchValue = document.querySelector("#concertSearch").value 
-//     // console.log(concertSearchValue)
-//     //         // Print 'results' to the DOM
-//         document.querySelector("#results").innerHTML =
-//         `<h4>Concerts in Nashville</h4>
-//         <p>${eventInfoArray._embedded.events}</p>`
-// });
 
 
 
