41b447df5ced408849aa4bf2b33a8e9b7b5e0ec4
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-05-01 10:02:39 -0400

    finally getting somewhere

--- concerts.js
+++ concerts.js
@@ -1,12 +1,27 @@
 //ticket master url https://app.ticketmaster.com/discovery/v2/
+//searched by city
 //api key Hftj2vZHCGI9rWySOyg1NifFCLWd7KMA
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=nashville&apikey=Hftj2vZHCGI9rWySOyg1NifFCLWd7KMA")
+    .then(response => response.json())
+    .then(eventInfoArray => {
+        console.log(eventInfoArray._embedded.events)
+    });
+
+    
 
 
 
 
+//add an event listener for the search text in id="concertSearch" button id="concertSearch-btn".  might need an if/else with statement for value typed into search.
+document.querySelector("#concertSearch-btn").addEventListener("click", function(){
+    // console.log("you clicked search button")
+    const concertSearchValue = document.querySelector("#concertSearch").value 
+    console.log(concertSearchValue)
+});
 
 
-//add an event listener for the search text in id="concerts-search" button id="concerts-search-btn".  might need an if/else with statement for value typed into search.
+
+// function printConcertSearch(){
 
 //fetch info from ticket master
 
--- domPrinter.js
+++ domPrinter.js
@@ -1,9 +1,15 @@
-//Search results printer
-function resultsPrinter(){
-    document.querySelector("#results").innerHTML = `<h2>Results</h2>
-    <div class="results-div" id="results-div"></div>
-    `
-}
+//Concert search printer
+function printConcertSearch(){
+    document.querySelector(".concerts-div").innerHTML += `<input type="search" placeholder="Search by Genre" id="concertSearch">
+    <button id="concertSearch-btn">Search</button>`
+    }
+    printConcertSearch();
+    
+// function resultsPrinter(){
+//     document.querySelector("#results").innerHTML = `<h2>Results</h2>
+//     <div class="results-div" id="results-div"></div>
+//     `
+// }
 
 //Itinerary printer
 function itinPrinter() {
