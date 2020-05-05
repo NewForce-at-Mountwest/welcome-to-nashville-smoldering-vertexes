7cd08fde6aa429210ba27de16833f7389fbcbf10
Your Name <you@example.com>
2020-05-05 13:28:23 -0400

    printer works

--- breweries.js
+++ breweries.js
@@ -1,30 +1,42 @@
-// https://www.openbrewerydb.org/
- 
-//fetching the information
-fetch("https://api.openbrewerydb.org/breweries?by_city=nashville")
-  .then((breweries) => breweries.json())
-  .then((parsedBrewriesArray) => {
-    console.log(parsedBrewriesArray);
+//PRINTS SEARCH BAR AND SEARCH BUTTON TO THE DOM
+document.querySelector(".breweries-div").innerHTML = `
+ <input id="brewInput" type="text" placeholder="Breweries by Name">
+ <button id="brewSearch">Search</button>
+ `;
 
-    parsedBrewriesArray.forEach(breweries => {
-        
-      if(parsedBrewriesArray.breweries){
+//PRINT SEARCH RESULTS TO THE DOM
+document.querySelector("body").addEventListener("click", function () {
+  if (event.target.id === "brewSearch") {
+    resultsPrinter();
 
-      }
-      if(){
+    const brewResults = document.querySelector("#brewInput").value;
+    console.log(brewResults);
+    brewerySearch(brewResults);
+  }
+});
 
-      }
-      if(){
+let brewButtonCounter = 0;
 
-      }
-      if(){
-
-      }
-      else {
-
-      }
-
+//Search Function
+function brewerySearch(name) {
+  fetch(
+    `https://api.openbrewerydb.org/breweries?by_city=nashville&by_state=tennessee&by_name=${name}`
+  )
+    .then((breweries) => breweries.json())
+    .then((parsedBrewriesArray) => {
+      parsedBrewriesArray.forEach((brewery) => {
+        console.log(brewery.name);
+        document.querySelector(
+          ".results-div"
+        ).innerHTML += `<h3 id="brewName${brewButtonCounter}">${brewery.name}</h3> <button id="saveButton" value=${brewButtonCounter}>save</button>`;
+        brewButtonCounter++;
+      });
     });
+}
 
-
-  });
+//adds click funtionality to save button
+document.querySelector("#results").addEventListener("click", function () {
+  console.log("save click");
+  itinPrinter();
+  document.querySelector("#itin-div").appendChild(document.querySelector(`#brewName${event.target.value}`))
+});
--- domPrinter.js
+++ domPrinter.js
@@ -1,15 +1,13 @@
 //Search results printer
-function resultsPrinter(){
-    document.querySelector("#results").innerHTML = `<h2>Results</h2>
+function resultsPrinter() {
+  document.querySelector("#results").innerHTML = `<h2>Results</h2>
     <div class="results-div" id="results-div"></div>
-    `
+    `;
 }
 
 //Itinerary printer
 function itinPrinter() {
-    document.querySelector("#itinerary").innerHTML = `<h2>My itinerary</h2>
-    <div class="itin-div" id="itin-div"></div>`
+  document.querySelector("#itinerary").innerHTML = `<h2>My itinerary</h2>
+    <div class="itin-div" id="itin-div"></div>`;
 }
-
-
 
